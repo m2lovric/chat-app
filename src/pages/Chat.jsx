ec89f880993cc2f7681addada69eefd4808b6f34
m2lovric <m2lovric@gmail.com>
2022-03-03 14:23:21 +0100

    Send message

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { db } from '../firebase/init';
 import Layout from './Layout';
-import { onSnapshot, collection } from 'firebase/firestore';
+import { onSnapshot, collection, addDoc } from 'firebase/firestore';
 
 const Chat = () => {
   const { id } = useParams();
   const [data, setData] = useState([]);
+  const [message, setMessage] = useState('');
+  const userId = 'matteo';
 
   useEffect(() => {
     onSnapshot(collection(db, id), (snapshot) => {
@@ -17,18 +19,41 @@ const Chat = () => {
     });
   }, []);
 
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+
+    addDoc(collection(db, id), {
+      date: new Date(),
+      message: message,
+      userId: 'matteo',
+    });
+    setMessage('');
+  };
+
   return (
     <Layout>
       <section className='App-chat'>
         {data.reverse().map((el, i) => {
           return (
-            <article key={i} className='App-chat-message'>
+            <article
+              key={i}
+              className={
+                el.userId === userId
+                  ? 'App-chat-message me'
+                  : 'App-chat-message'
+              }
+            >
               <p>{el.message}</p>
             </article>
           );
         })}
-        <form className='App-chat-input'>
-          <input type='text' className='input' />
+        <form className='App-chat-input' onSubmit={handleSendMessage}>
+          <input
+            type='text'
+            className='input'
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
           <input
             type='image'
             src='/src/images/icons8-send-64.png'
